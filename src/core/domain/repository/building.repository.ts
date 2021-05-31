07e3649369accfd0a8a083b6d1c9e8444408e1c0
NikNikich <nikolai.andreev@citronium.com>
2021-05-31 14:09:33 +0300

    Done

--- src/core/domain/repository/building.repository.ts
+++ src/core/domain/repository/building.repository.ts
@@ -1,12 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Building } from '../entity/building.entity';
-import { DadataDataDto } from '../../../infrastructure/presenter/rest-api/documentation/user/dadata.data.dto';
 import { ErrorIf } from '../../../infrastructure/presenter/rest-api/errors/error.if';
 import { INVALID_ADDRESS_DATA } from '../../../infrastructure/presenter/rest-api/errors/errors';
+import { DadataObjectDto } from '../../../infrastructure/presenter/rest-api/documentation/user/dadata.object.dto';
 
 @EntityRepository(Building)
 export class BuildingRepository extends Repository<Building> {
-  async getBuildingByAddress(dadataData: DadataDataDto): Promise<Building> {
+  async getBuildingByAddress(dadataObject: DadataObjectDto): Promise<Building> {
+    const dadataData = dadataObject.data;
     const dataOk: boolean =
       !!dadataData &&
       !!dadataData.city_with_type &&
@@ -38,6 +39,7 @@ export class BuildingRepository extends Repository<Building> {
     newBuilding.house = dadataData.house;
     newBuilding.geoLon = dadataData.geo_lon;
     newBuilding.geoLat = dadataData.geo_lat;
+    newBuilding.dadataJson = dadataObject;
     return newBuilding.save();
   }
 }
