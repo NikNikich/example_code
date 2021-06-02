667abc5b1d247b2f3043577ff4788c46b4fe583c
NikNikich <nikolai.andreev@citronium.com>
2021-06-02 13:02:14 +0300

    edit add building

--- src/core/domain/repository/building.repository.ts
+++ src/core/domain/repository/building.repository.ts
@@ -9,23 +9,12 @@ export class BuildingRepository extends Repository<Building> {
   async getBuildingByAddress(dadataObject: DadataObjectDto): Promise<Building> {
     const dadataData = dadataObject.data;
     const dataOk: boolean =
-      !!dadataData &&
-      !!dadataData.city_with_type &&
-      !!dadataData.region_with_type &&
-      !!dadataData.street_with_type &&
-      !!dadataData.country &&
-      !!dadataData.house;
+      !!dadataData && !!dadataData.geo_lat && !!dadataData.geo_lon;
     ErrorIf.isFalse(dataOk, INVALID_ADDRESS_DATA);
     const findOption = {
-      country: dadataData.country,
-      region: dadataData.region_with_type,
-      city: dadataData.city_with_type,
-      street: dadataData.street_with_type,
-      house: dadataData.house,
+      geoLat: dadataData.geo_lat,
+      geoLon: dadataData.geo_lon,
     };
-    if (dadataData.area_with_type) {
-      Object.assign(findOption, { area: dadataData.area_with_type });
-    }
     const findBuilding = await this.findOne(findOption);
     if (findBuilding) {
       return findBuilding;
