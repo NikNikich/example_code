913b7a1c072530408842e80e13a03c6bfd42f3b2
NikNikich <nikolai.andreev@citronium.com>
2021-06-23 15:56:13 +0300

    edit view roles

--- src/infrastructure/shared/constants.ts
+++ src/infrastructure/shared/constants.ts
@@ -23,7 +23,6 @@ export const VISIBLE_ROLES = (): { [name: string]: UserRolesEnum[] } => {
   visible[UserRolesEnum.DEALER] = [
     UserRolesEnum.DEALER_SERVICE,
     UserRolesEnum.CLIENT,
-    UserRolesEnum.CLIENT_SERVICE,
   ];
   visible[UserRolesEnum.CLIENT] = [UserRolesEnum.CLIENT_SERVICE];
   return visible;
